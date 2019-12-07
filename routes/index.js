6d8e60b9921fb1b320232090cdda96b75553bbb9
guilmarc <hk78@live.ca>
2019-12-07 09:56:36 -0500

    Tweet WIP

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const api = require('./api')
+const api = require('./api');
+const Tweet = require('../database/models/tweet.model');
 
 router.use('/api', api)
 
@@ -8,7 +9,8 @@ router.get('/tweet/new', (req, res) => {
 });
 
 router.get("/", (req, res) => {
-   res.render("tweets/tweet-list");
+   Tweet.find({}).exec()
+      .then((tweets)=>{ res.render("tweets/tweet-list", { tweets }); })
 });
 
 module.exports = router;
