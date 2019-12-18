7300fc033ffc25619df176547b2a8a4d14f9c9d3
Marco Guilmette <marco.guilmette@softbiomed.com>
2019-12-18 17:31:19 -0500

    Tweeter 4 WIP

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,17 +1,29 @@
 //const queries = require('../queries/tweets.queries');
-
+const passport = require('passport');
 exports.signinForm = (req, res, next) => {
   try {
-      res.render('signin-form', {errors: null});
+      res.render('auth/auth-form', {errors: null});
   } catch(e) {
     next(e);
   }
 };
 
 exports.signin = (req, res, next) => {
-  res.end();
+  passport.authenticate('local', (err, user, info) => {
+    if(err) {
+      next(err);
+    } else if (!user) {
+      res.render('auth/auth-form', { errors: [info.message] });
+    } else {
+      req.login(user, (err) => {
+        if(err) next(err);
+        else res.redirect('/tweets');
+      });
+    }
+  })(req, res, next)
 };
 
 exports.signout = (req, res, next) => {
-  res.end();
+  req.logout();
+  res.redirect('/auth/signin/form');
 };
